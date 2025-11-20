304ba26de32c358f175e95d1237058107b815293
AnSL-Ro <25460336@colima.tecnm.mx>
2025-11-20 17:59:54 -0600

    Se agrega screen.py para la interfaz y spam_detetctor

---
+++ source/screen.py
@@ -0,0 +1,50 @@
+import customtkinter as ctk
+import spam_detector
+
+def analizar_email():
+    texto = entrada_email.get()
+
+    if texto == "":
+        etiqueta_resultado.configure(text="Porfavor escribe un email")
+        return
+    resultado = spam_detector.predecir_spam(texto, modelo, vectorizer)
+
+    if resultado == "SPAM":
+        etiqueta_resultado.configure(text="SPAM", fg_color="red")
+    else:
+        etiqueta_resultado.configure(text="NO SPAM", fg_color="green")
+
+def main():
+    global modelo, vectorizer, entrada_email, etiqueta_resultado
+
+    print("Entrenando modelo...")
+    modelo, vectorizer = spam_detector.entrenar_modelo()
+
+    accuary = spam_detector.evaluar_modelo(modelo, vectorizer)
+    print(f"Precision del modelo: {accuary:.2f}")
+
+    ventana = ctk.CTk()
+    ventana.title("Detector de spam")
+    ventana.geometry("500x300")
+
+    titulo = ctk.CTkLabel(ventana, text="Detector de spam", font=("Arial", 20, "bold"))
+    titulo.pack(pady=20)
+
+    instruccion = ctk.CTkLabel(ventana, text="Escribe un email para analizar")
+    instruccion.pack()
+
+    entrada_email = ctk.CTkEntry(ventana, width=400)
+    entrada_email.pack(pady=10)
+
+    boton = ctk.CTkButton(ventana, text="Analizar", command=analizar_email, text_color="white", fg_color="#4CAF50", hover_color="#4D8150")
+    boton.pack(pady=10)
+
+    etiqueta_resultado = ctk.CTkLabel(ventana, text="", font=("Arial", 14))
+    etiqueta_resultado.pack(pady=20)
+
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+                          
--- source/v_Tkinter.py
+++ source/v_Tkinter.py
@@ -1,19 +1,28 @@
-import tkinter as tk
+
+import customtkinter as ctk
 from tkinter import messagebox
 
-def mostrar_mensaje():
-    messagebox.showinfo("Aviso", "¡Has hecho clic en el botón!")
+def main():
 
-ventana = tk.Tk()
-ventana.title("Ventana simple")
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
 
-label = tk.Label(ventana, text="¡Hola, mundo!")
-label.pack(pady=10)
+    def mostrar_mensaje():
+        
+        messagebox.showinfo("Aviso", "Escaneo de spam completado")
 
-boton = tk.Button(ventana, text="haz clic aqui", command=mostrar_mensaje)
-boton.pack(pady=10)
+    ventana = ctk.CTk()
+    ventana.title("Spam detetctor")
+    ventana.geometry("600x400")
 
+    label = ctk.CTkLabel(ventana, text="Analizador de spam", font=("Arial", 20, "bold"))
 
+    label.pack(pady=10)
 
-ventana.mainloop()
+    boton = ctk.CTkButton(ventana, text="Analizar Spam", fg_color= "green", command=mostrar_mensaje, hover_color="red")
+    boton.pack(pady=10)
 
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
